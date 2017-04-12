b1a9659aae467e8369a7771b8f963c59e0284919
prashanthm121 <prashanth.m0101@Gmail.com>
2017-04-12 21:24:31 +0530

    cleverbot

---
+++ CleverbotModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace SadistDiscordBotCore.Modules.Cleverbot
+{
+	public class CleverbotModule : ModuleBase<SocketCommandContext>
+    {
+		[Command("c")]
+		public async Task Speak([Remainder]string message)
+		{
+			CleverbotSession cleverbot = new CleverbotSession("");
+			var response = await cleverbot.GetResponseAsync(message);
+			await ReplyAsync(response.Response);
+		}
+    }
+}
